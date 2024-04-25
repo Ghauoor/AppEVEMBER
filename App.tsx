bb225ba0343a70d4ecc5d15e6d40c7bfaa91c70f
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-04-26 04:54:58 +0500

    custom fonts added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from "react-native";
 import DayListItem from "./src/components/core/DayListItem";
+import React, { useEffect } from "react";
+import * as SplashScreen from 'expo-splash-screen';
+
+import { useFonts, AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc';
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Amatic: AmaticSC_400Regular,
+    AmaticSC: AmaticSC_700Bold
+  })
+  const loadFonst = async () =>
+    await SplashScreen.preventAutoHideAsync()
+
+  useEffect(() => {
+    if (fontsLoaded || fontError)
+      loadFonst
+
+  }, [fontsLoaded, fontError])
+
+  if (!fontsLoaded && !fontError) {
+    console.log("Hello There");
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <FlatList
---
+++ utils.ts
@@ -0,0 +1,4 @@
+const formateNumber = (val: number) => {
+
+
+}
