d6b9871ace0c0a0e2f9767483a7e63bf9d2696c9
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-04-27 05:21:48 +0500

    Load fonts in root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,27 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+
+import {
+  useFonts,
+  AmaticSC_400Regular,
+  AmaticSC_700Bold,
+} from "@expo-google-fonts/amatic-sc";
+import React from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Amatic: AmaticSC_400Regular,
+    AmaticSC: AmaticSC_700Bold,
+  });
+  const loadFonts = async () => await SplashScreen.preventAutoHideAsync();
+
+  React.useEffect(() => {
+    if (fontsLoaded || fontError) loadFonts;
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <Stack screenOptions={{}}>
       <Stack.Screen name="index" options={{ title: "AppEMBER" }} />
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,38 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  ActivityIndicator,
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import DayListItem from "../components/core/DayListItem";
 import React from "react";
-import * as SplashScreen from "expo-splash-screen";
-
-import {
-  useFonts,
-  AmaticSC_400Regular,
-  AmaticSC_700Bold,
-} from "@expo-google-fonts/amatic-sc";
 
 const days = [...Array(24)].map((_, index) => index + 1);
 
 export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
-    Amatic: AmaticSC_400Regular,
-    AmaticSC: AmaticSC_700Bold,
-  });
-  const loadFonts = async () => await SplashScreen.preventAutoHideAsync();
-
-  React.useEffect(() => {
-    if (fontsLoaded || fontError) loadFonts;
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
   return (
     <View style={styles.container}>
       <FlatList
